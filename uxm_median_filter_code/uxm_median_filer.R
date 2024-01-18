4ae0de4613475715263033b2fdfffb27a1fd8918
jinqiyuan1 <139473899+jinqiyuan1@users.noreply.github.com>
2024-01-18 23:08:43 +0800

    Add files via upload

---
+++ uxm_median_filter_code/uxm_median_filer.R
@@ -0,0 +1,57 @@
+sort<-read.table("/home/yjq/UXM_deconv/UXM/uniform_distribution/filter_15_markers/filter_15_markers.bed",header = FALSE , sep = '\t')
+colnames(sort)<-c("chr","start","end","startCpG","endCpG","target","name","lenCpG","bp","tg_mean","bg_mean","delta_means","delta_quants","delta_maxmin","ttest","direction")
+write.table(sort,"/home/yjq/UXM_deconv/UXM/uniform_distribution/filter_15_markers/filter_15_markers.bed",row.names = FALSE,col.names = TRUE,quote = F,sep = "\t")
+
+sort<-read.table("/home/yjq/UXM_deconv/UXM/uniform_distribution/filter_20_markers/filter_20_markers.bed",header = FALSE , sep = '\t')
+colnames(sort)<-c("chr","start","end","startCpG","endCpG","target","name","lenCpG","bp","tg_mean","bg_mean","delta_means","delta_quants","delta_maxmin","ttest","direction")
+write.table(sort,"/home/yjq/UXM_deconv/UXM/uniform_distribution/filter_20_markers/filter_20_markers.bed",row.names = FALSE,col.names = TRUE,quote = F,sep = "\t")
+
+
+sort<-read.table("/home/yjq/UXM_deconv/UXM/uniform_distribution/filter_25_markers/filter_25_markers.bed",header = FALSE , sep = '\t')
+colnames(sort)<-c("chr","start","end","startCpG","endCpG","target","name","lenCpG","bp","tg_mean","bg_mean","delta_means","delta_quants","delta_maxmin","ttest","direction")
+write.table(sort,"/home/yjq/UXM_deconv/UXM/uniform_distribution/filter_25_markers/filter_25_markers.bed",row.names = FALSE,col.names = TRUE,quote = F,sep = "\t")
+
+sort<-read.table("/home/yjq/UXM_deconv/UXM/uniform_distribution/filter_30_markers/filter_30_markers.bed",header = FALSE , sep = '\t')
+colnames(sort)<-c("chr","start","end","startCpG","endCpG","target","name","lenCpG","bp","tg_mean","bg_mean","delta_means","delta_quants","delta_maxmin","ttest","direction")
+write.table(sort,"/home/yjq/UXM_deconv/UXM/uniform_distribution/filter_30_markers/filter_30_markers.bed",row.names = FALSE,col.names = TRUE,quote = F,sep = "\t")
+
+sort<-read.table("/home/yjq/UXM_deconv/UXM/uniform_distribution/UXM/filter_35_markers/filter_35_markers.bed",header = FALSE , sep = '\t')
+colnames(sort)<-c("chr","start","end","startCpG","endCpG","target","name","lenCpG","bp","tg_mean","bg_mean","delta_means","delta_quants","delta_maxmin","ttest","direction")
+write.table(sort,"/home/yjq/UXM_deconv/UXM/uniform_distribution/UXM/filter_35_markers/filter_35_markers.bed",row.names = FALSE,col.names = TRUE,quote = F,sep = "\t")
+
+sort<-read.table("/home/yjq/UXM_deconv/UXM/uniform_distribution/filter_40_markers/filter_40_markers.bed",header = FALSE , sep = '\t')
+colnames(sort)<-c("chr","start","end","startCpG","endCpG","target","name","lenCpG","bp","tg_mean","bg_mean","delta_means","delta_quants","delta_maxmin","ttest","direction")
+write.table(sort,"/home/yjq/UXM_deconv/UXM/uniform_distribution/filter_40_markers/filter_40_markers.bed",row.names = FALSE,col.names = TRUE,quote = F,sep = "\t")
+
+sort<-read.table("/home/yjq/UXM_deconv/UXM/filter_45_markers/filter_45_markers.bed",header = FALSE , sep = '\t')
+colnames(sort)<-c("chr","start","end","startCpG","endCpG","target","name","lenCpG","bp","tg_mean","bg_mean","delta_means","delta_quants","delta_maxmin","ttest","direction")
+write.table(sort,"/home/yjq/UXM_deconv/UXM/filter_45_markers/filter_45_markers.bed",row.names = FALSE,col.names = TRUE,quote = F,sep = "\t")
+
+sort<-read.table("/home/yjq/UXM_deconv/UXM/uniform_distribution/filter_50_markers/filter_50_markers.bed",header = FALSE , sep = '\t')
+colnames(sort)<-c("chr","start","end","startCpG","endCpG","target","name","lenCpG","bp","tg_mean","bg_mean","delta_means","delta_quants","delta_maxmin","ttest","direction")
+write.table(sort,"/home/yjq/UXM_deconv/UXM/uniform_distribution/filter_50_markers/filter_50_markers.bed",row.names = FALSE,col.names = TRUE,quote = F,sep = "\t")
+
+sort<-read.table("/home/yjq/UXM_deconv/UXM/uniform_distribution/filter_60_markers/filter_60_markers.bed",header = FALSE , sep = '\t')
+colnames(sort)<-c("chr","start","end","startCpG","endCpG","target","name","lenCpG","bp","tg_mean","bg_mean","delta_means","delta_quants","delta_maxmin","ttest","direction")
+write.table(sort,"/home/yjq/UXM_deconv/UXM/uniform_distribution/filter_60_markers/filter_60_markers.bed",row.names = FALSE,col.names = TRUE,quote = F,sep = "\t")
+
+sort<-read.table("/home/yjq/UXM_deconv/UXM/uniform_distribution/filter_70_markers/filter_70_markers.bed",header = FALSE , sep = '\t')
+colnames(sort)<-c("chr","start","end","startCpG","endCpG","target","name","lenCpG","bp","tg_mean","bg_mean","delta_means","delta_quants","delta_maxmin","ttest","direction")
+write.table(sort,"/home/yjq/UXM_deconv/UXM/uniform_distribution/filter_70_markers/filter_70_markers.bed",row.names = FALSE,col.names = TRUE,quote = F,sep = "\t")
+
+sort<-read.table("/home/yjq/UXM_deconv/UXM/uniform_distribution/filter_80_markers/filter_80_markers.bed",header = FALSE , sep = '\t')
+colnames(sort)<-c("chr","start","end","startCpG","endCpG","target","name","lenCpG","bp","tg_mean","bg_mean","delta_means","delta_quants","delta_maxmin","ttest","direction")
+write.table(sort,"/home/yjq/UXM_deconv/UXM/uniform_distribution/filter_80_markers/filter_80_markers.bed",row.names = FALSE,col.names = TRUE,quote = F,sep = "\t")
+
+sort<-read.table("/home/yjq/UXM_deconv/UXM/uniform_distribution/filter_90_markers/filter_90_markers.bed",header = FALSE , sep = '\t')
+colnames(sort)<-c("chr","start","end","startCpG","endCpG","target","name","lenCpG","bp","tg_mean","bg_mean","delta_means","delta_quants","delta_maxmin","ttest","direction")
+write.table(sort,"/home/yjq/UXM_deconv/UXM/uniform_distribution/filter_90_markers/filter_90_markers.bed",row.names = FALSE,col.names = TRUE,quote = F,sep = "\t")
+
+sort<-read.table("/home/yjq/UXM_deconv/UXM/uniform_distribution/filter_100_markers/filter_100_markers.bed",header = FALSE , sep = '\t')
+colnames(sort)<-c("chr","start","end","startCpG","endCpG","target","name","lenCpG","bp","tg_mean","bg_mean","delta_means","delta_quants","delta_maxmin","ttest","direction")
+write.table(sort,"/home/yjq/UXM_deconv/UXM/uniform_distribution/filter_100_markers/filter_100_markers.bed",row.names = FALSE,col.names = TRUE,quote = F,sep = "\t")
+
+sort<-read.table("/home/yjq/UXM_deconv/UXM/uniform_distribution/filter_20_markers/filter_20_markers.bed",header = FALSE , sep = '\t')
+sort<- sort[-1,]
+colnames(sort)<-c("chr","start","end","startCpG","endCpG","target","name","lenCpG","bp","tg_mean","bg_mean","delta_means","delta_quants","delta_maxmin","ttest","direction")
+write.table(sort,"/home/yjq/UXM_deconv/UXM/uniform_distribution/filter_20_markers/filter_20_markers.bed",row.names = FALSE,col.names = TRUE,quote = F,sep = "\t")
