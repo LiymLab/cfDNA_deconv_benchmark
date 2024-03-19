6535a9a721b68610442645f6b19adde703daa3c8
yacheng_zhu <130423127+geminizyc@users.noreply.github.com>
2024-03-19 19:54:33 +0800

    Delete celfeer_work directory

--- celfeer_work/celfeer_crd/celfeer_drop_tissue/celfeer_drop_tissue_make_crd_matrix.R
+++
@@ -1,8 +0,0 @@
-crd_matrix <- read.csv('/home/sty/cfDNA_benchmark/benchmark_pat/crd_dis/crd_dis.csv')
-
-#采用循环依次从完整的分布数据中去除对应tissue的数据信息得到比较所需的数据
-for(i in c('Endothelium','Blood-Granul','Adipocytes','Blood-NK','Head-Neck-Ep')){
-  crd_matrix_drop <-  crd_matrix[-which(crd_matrix$ID == i),]     #删除对应tissue的索引的数据
-  data_name <- paste0('/home/zyc/celfeer_work2/celfeer_drop_tissue/data_median_15_drop_',i,'/crd_matrix.csv')
-  write.table(crd_matrix_drop,file = data_name,quote = FALSE,col.names = TRUE,row.names = FALSE,sep = ',')
-}
