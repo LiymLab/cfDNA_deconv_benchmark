487d184be5a6aedd2922c53bed6d143be403915c
yacheng_zhu <130423127+geminizyc@users.noreply.github.com>
2024-03-19 19:54:45 +0800

    Delete celfie_work directory

--- celfie_work/celfie_uniform/celfie_drop_tissue/make_uniform_matrix.R
+++
@@ -1,8 +0,0 @@
-uniform_matrix <- read.csv('/home/sty/cfDNA_benchmark/benchmark_pat/uniform_dis/uniform_matrix_d.csv')
-
-for(i in c('Endothelium','Blood-Granul','Adipocytes','Blood-NK','Head-Neck-Ep')){
-  uniform_matrix_drop <-  uniform_matrix[-which(uniform_matrix$ID == i),]
-  data_name <- paste0('~/celfie_drop_tissue/data_median_15_drop_',i,'/uniform_matrix.csv')
-  write.table(uniform_matrix_drop,file = data_name,quote = FALSE,col.names = TRUE,row.names = FALSE,sep = ',')
-}
-
