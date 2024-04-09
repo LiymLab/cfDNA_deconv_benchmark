a44923be0e20abb8071c3b063712c6d4524b8e11
lymlab-bcb <154315044+LiymLab@users.noreply.github.com>
2024-04-09 21:30:45 +0800

    Delete Depth_filter&markers_selection directory

--- Depth_filter&markers_selection/CelFiE/crd_distribution/celfie_median_depth_filter.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-for i in 15 20 25 30 35 40 45 50 60 70 80 90 100
-do
-	#use samples_summed.R to sum methylation data and get complete file for deconvolution
-	Rscript ./samples_summed.R ~/celfie_work1/median_${i}/bedtools_res_median_${i}.bed ~/celfie_work1/median_${i}/sample_input_tims_summed_median_${i}.txt ~/celfie_work2/median_depth_filter/median_${i}/data_input_median_${i}.txt
-done
-
-
