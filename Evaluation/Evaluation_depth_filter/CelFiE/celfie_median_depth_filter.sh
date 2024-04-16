329e77af8cef69f98931608c3243a4b21d140716
yacheng_zhu <130423127+geminizyc@users.noreply.github.com>
2024-04-16 17:45:32 +0800

    Update celfie_median_depth_filter.sh

--- Evaluation/Evaluation_depth_filter/CelFiE/celfie_median_depth_filter.sh
+++ Evaluation/Evaluation_depth_filter/CelFiE/celfie_median_depth_filter.sh
@@ -10,8 +10,8 @@ workplace = 'path/workplace' # You can set your own work path
 
 for i in 15 20 25 30 35 40 45 50 60 70 80 90 100
 do
-	# get data of 250bps around selected markers
-	cut -f 1-3 ${workplace}/median_${i}/sample_input_tims_summed_median_${i}.txt > ${workplace}/median_${i}/tims_median_${i}.bed
+    # get data of 250bps around selected markers
+    cut -f 1-3 ${workplace}/median_${i}/sample_input_tims_summed_median_${i}.txt > ${workplace}/median_${i}/tims_median_${i}.bed
 
     bedtools intersect -a ${workplace}/median_${i}/tims_median_${i}.bed -b ${workplace}/bed.bed -wa -wb > ${workplace}/median_${i}/bedtools_res_median_${i}.bed
 
@@ -19,7 +19,7 @@ do
     Rscript ./samples_summed.R ${workplace}/median_${i}/bedtools_res_median_${i}.bed ${workplace}/median_${i}/sample_input_tims_summed_median_${i}.txt ${workplace}/median_${i}/data_input_median_${i}.txt
 
     #use celfie.py for deconvolution
-	python celfie/scripts/celfie.py ${workplace}/median_${i}/data_input_median_${i}.txt ${workplace}/median_${i}/sample_output_median_${i} 100 -u 0
+    python celfie/scripts/celfie.py ${workplace}/median_${i}/data_input_median_${i}.txt ${workplace}/median_${i}/sample_output_median_${i} 100 -u 0
 done
 
 
