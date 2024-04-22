62a455b49f8c3a7218fb715ca91c6979c3577c2a
lymlab-bcb <154315044+LiymLab@users.noreply.github.com>
2024-04-22 16:30:53 +0800

    Add files via upload

---
+++ Evaluation/Evaluation_markers_selection/CelFiE/celfie_median_depth_filter.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+##CelFiE_workflow##
+# Evaluation of sequencing depth filter thresholds in the reference methylation atlas
+# For data of different distributions, the workflow is the same
+
+workplace = 'path/workplace' # You can set your own work path
+
+# Then, modify parameters including input_file, output_file, summed_file, number_tissues and depth_filter in tim.sh which CelFiE offers according to actual demands to generate reference methylation atlas
+
+for i in 15 20 25 30 35 40 45 50 60 70 80 90 100
+do
+    # get data of 250bps around selected markers
+    cut -f 1-3 ${workplace}/median_${i}/sample_input_tims_summed_median_${i}.txt > ${workplace}/median_${i}/tims_median_${i}.bed
+
+    bedtools intersect -a ${workplace}/median_${i}/tims_median_${i}.bed -b ${workplace}/bed.bed -wa -wb > ${workplace}/median_${i}/bedtools_res_median_${i}.bed
+
+    # use samples_summed.R to sum methylation data and get complete file for deconvolution
+    Rscript ./samples_summed.R ${workplace}/median_${i}/bedtools_res_median_${i}.bed ${workplace}/median_${i}/sample_input_tims_summed_median_${i}.txt ${workplace}/median_${i}/data_input_median_${i}.txt
+
+    #use celfie.py for deconvolution
+    python celfie/scripts/celfie.py ${workplace}/median_${i}/data_input_median_${i}.txt ${workplace}/median_${i}/sample_output_median_${i} 100 -u 0
+done
+
+
---
+++ Evaluation/Evaluation_markers_selection/CelFiE/samples_summed.R
@@ -0,0 +1,56 @@
+library(data.table)
+
+#bedtools intersect -a tims_bed.bed -b bed.bed -wa -wb > bedtools_res_new.bed
+
+args <- commandArgs(trailingOnly = TRUE)
+input1 <- args[1]
+input2 <- args[2]
+output <- args[3]
+
+file_names <- list.files('/home/sty/cfDNA_benchmark/benchmark_pat/pat_merged/ref/') # path storing reference data; change the path according to your actual situation
+file_names <- file_names[grep('.beta',file_names)]
+tissues <- sapply(strsplit(file_names,'\\.'),'[',1)
+colnames_tissues_res <- c()
+for (i in 1:length(file_names)){
+  tissue_meth <- paste0(tissues[i],'_METH')
+  tissue_depth <- paste0(tissues[i],'_DEPTH')
+  colnames_tissues_res <- c(colnames_tissues_res,tissue_meth,tissue_depth)
+}
+
+bedtools_res <- read.table(input1)
+tims_summed <- read.table(input2)
+tims <- tims_summed[1:3]
+
+file_names <- list.files("/home/sty/cfDNA_benchmark/benchmark_pat/pat_merged/uniform_dis/") # path storing sample data; change the path according to your actual situation
+file_names <- file_names[grep('.beta',file_names)]
+cpgs_all <- as.data.frame(matrix(ncol = 0,nrow = nrow(tims)))
+colnames_samples_res <- c()
+for (j in 1:length(file_names)){
+  fname <- file_names[j]
+  N <- file.info(fname)$size
+  content <- matrix(readBin(fname, "integer", N, size = 1, signed = FALSE), N / 2, 2, byrow=TRUE)
+  sample_meth <- paste0('sample',j,'_METH')
+  sample_depth <- paste0('sample',j,'_DEPTH')
+  colnames_samples_res <- c(colnames_samples_res,sample_meth,sample_depth)
+  cpgs_res <- data.frame()
+  for (k in 1:dim(tims)[1]){
+    region_new <- tims[k,]
+    find_index_new <- subset(bedtools_res,V1==region_new[,1] & V2==region_new[,2] & V3==region_new[,3])
+    index_new <- find_index_new$V7
+    if(length(index_new)==1){
+      cpgs_new <- transpose(as.data.frame(content[index_new,]))
+    }else{
+      cpgs_new <- as.data.frame(content[index_new,])
+    }
+    cpgs_summed_new <- transpose(as.data.frame(colSums(cpgs_new)))
+    cpgs_res <- rbind(cpgs_res,cpgs_summed_new)
+  }
+  cpgs_all <- cbind(cpgs_all,cpgs_res)
+}
+
+samples_data <- cpgs_all
+tissues_data <- tims_summed
+colnames(tissues_data) <- c('CHROM','START','END',colnames_tissues_res)
+colnames(samples_data) <- colnames_samples_res
+data_input <- cbind(tissues_data[,c(1:3)],samples_data,tissues_data)
+write.table(data_input,file = output,quote = FALSE,row.names = FALSE,col.names = TRUE,sep = '\t')
