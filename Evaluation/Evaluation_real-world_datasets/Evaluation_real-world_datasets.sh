6f62948c12c0ceae158c341738fae4da793e21f1
lymlab-bcb <154315044+LiymLab@users.noreply.github.com>
2024-11-13 11:29:03 +0800

    Update Evaluation_real-world_datasets.sh

--- Evaluation/Evaluation_real-world_datasets/Evaluation_real-world_datasets.sh
+++ Evaluation/Evaluation_real-world_datasets/Evaluation_real-world_datasets.sh
@@ -1,2 +1,2 @@
-You can use the scripts in the Deconvolution folder to run and obtain the cell type proportion files for the five methods under real data conditions.
+#You can use the scripts in the Deconvolution folder to run and obtain the cell type proportion files for the five methods under real data conditions.
 
