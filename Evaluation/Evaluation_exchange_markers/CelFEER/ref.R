573562b43ecf8ead2f1750aa6b97646a9c4379a4
yacheng_zhu <130423127+geminizyc@users.noreply.github.com>
2024-12-07 20:13:08 +0800

    Update ref.R

--- Evaluation/Evaluation_exchange_markers/CelFEER/ref.R
+++ Evaluation/Evaluation_exchange_markers/CelFEER/ref.R
@@ -33,7 +33,7 @@ for (i in 1:length(file_names)){
   file_name <- paste0(file_path,file_names[i])
   tissue_onehot <- fread(file_name,skip = 1)
   colnames(tissue_onehot) <- c('index','one','two','three','four','five')
-  tissue_onehot <- tissue_onehot[,.(sum(one),sum(two),sum(three),sum(four),sum(five)),by=index] #加和每个site的数据
+  tissue_onehot <- tissue_onehot[,.(sum(one),sum(two),sum(three),sum(four),sum(five)),by=index] 
   data <- merge(bin_index,tissue_onehot,by = 'index',all.x = TRUE) 
   data[is.na(data)] <- 0
   data_res <- data[,c(5:9)]
